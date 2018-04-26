27ae7e80a0dec3f154e39eb355164502a9f4187a
Muneeb Aadil <imuneebaadil@gmail.com>
2018-04-26 15:16:14 +0500

    changed code directory structure temporarily

---
+++ code/slave/slave.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	files, err := ioutil.ReadDir("../data/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
+}
