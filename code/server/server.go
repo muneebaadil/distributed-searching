febec68287cb7fde47b54c3f89d2ac708e79c646
Muneeb Aadil <imuneebaadil@gmail.com>
2018-04-26 17:47:53 +0500

    server: updating filesLog on incoming slaves

--- code/server/server.go
+++ code/server/server.go
@@ -1,7 +1,81 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+// key value pair of (filename, list of connections)
+var filesLog = make(map[string][]net.Conn)
+
+func handleSlaves(conn net.Conn) {
+	//first reading list of chunks from the newly arrived slave
+	buffer := make([]byte, 4096)
+	n, _ := conn.Read(buffer)
+	newMsg := string(buffer[:n])
+	fileNames := strings.Split(newMsg, " ")
+
+	//updating filesLog
+	for _, fileName := range fileNames {
+		conns, ok := filesLog[fileName]
+
+		fmt.Printf("adding %s file\n", fileName)
+
+		if ok == false { //if no such file is held by any slave
+			toAdd := []net.Conn{conn}
+			filesLog[fileName] = toAdd
+
+		} else { //if the file is already held by some some slave(s)
+			conns = append(conns, conn)
+			filesLog[fileName] = conns
+		}
+	}
+}
+
+func listenForSlaves(portNum string) {
+	ln, err := net.Listen("tcp", ":"+portNum)
+	log.Printf("Listening for slaves on %s", portNum)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for true {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Fatal(err)
+			continue
+		}
+
+		go handleSlaves(conn)
+	}
+}
+
+func handleClients(conn net.Conn) {
+
+}
+
+func listenForClients(portNum string) {
+	ln, err := net.Listen("tcp", ":"+portNum)
+	log.Printf("Listening for clients on %s", portNum)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for true {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Fatal(err)
+			continue
+		}
+
+		go handleClients(conn)
+	}
+}
 
 func main() {
-	fmt.Printf("Helloworld!\n")
+
+	go listenForSlaves("1234")
+	listenForClients("1235")
 }
--- code/slave/slave.go
+++ code/slave/slave.go
@@ -4,14 +4,33 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
 )
 
-func main() {
-	files, err := ioutil.ReadDir("../data/")
+func handleConnection(conn net.Conn) {
+	//reading filenames first to update the server
+	files, err := ioutil.ReadDir("../../data/")
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	toSend := ""
 	for _, file := range files {
-		fmt.Println(file.Name())
+		toSend += " " + file.Name()
+	}
+	toSend = toSend[1:]
+
+	fmt.Printf("message sent to server: %s", toSend)
+	conn.Write([]byte(toSend))
+}
+
+func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:1234")
+
+	if err != nil {
+		fmt.Printf("error encountered with connection setup")
+
+	} else {
+		handleConnection(conn)
 	}
 }
